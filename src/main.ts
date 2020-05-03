ad4173101bcec964886f3a4116e65430bd922229
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-03 15:09:10 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,20 @@ const createWindow = () => {
   })
 
   win.loadFile(path.join(__dirname, '../index.html'))
+
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})

51fdd577b5cae5e032b54efc7e894f02266b4fbc
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-03 15:04:25 +0900

    modified environment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,7 +10,7 @@ const createWindow = () => {
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile(path.join(__dirname, '../index.html'))
 }
 
 app.whenReady().then(createWindow)
